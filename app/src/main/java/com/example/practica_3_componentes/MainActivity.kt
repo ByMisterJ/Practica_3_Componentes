320da2a131a1040fc8ca435d80af913494d0aaaa
josarapar <josearanda9000@gmail.com>
2025-10-16 16:52:46 +0200

    Cambios practicos por motivos de errores

--- app/src/main/java/com/example/practica_3_componentes/MainActivity.kt
+++ app/src/main/java/com/example/practica_3_componentes/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.practica_3_componentes
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Referencia al botón
+        val button1 = findViewById<Button>(R.id.button1)
+
+        // Al hacer clic, ir a la nueva Activity
+        button1.setOnClickListener {
+            val intent = Intent(this, NewPlayer::class.java)
+            startActivity(intent)
         }
     }
 }
